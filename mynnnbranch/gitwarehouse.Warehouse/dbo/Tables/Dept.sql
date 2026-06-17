43867fd995b130b252c5afebe8b0722e5d0f1162
PremSharma2025 <46988451+PremSharma2025@users.noreply.github.com>
2026-06-17 18:40:23 +0530

    Committing 1 item from workspace 252571b7-b9b3-4c27-8f0f-b7866132ef04

---
+++ mynnnbranch/gitwarehouse.Warehouse/dbo/Tables/Dept.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Dept] (
+
+	[deptno] int NULL, 
+	[dname] varchar(10) NULL, 
+	[sal] int NULL, 
+	[Deptno] int NULL
+);
---
+++ mynnnbranch/gitwarehouse.Warehouse/dbo/Tables/emp.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[emp] (
+
+	[id] int NULL, 
+	[ename] varchar(10) NULL, 
+	[sal] int NULL, 
+	[Deptno] int NULL
+);
